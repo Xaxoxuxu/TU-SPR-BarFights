5c668ace2322ff5aabb69ecea5b2a262ed995cb4
Antonio <tonkatapk1@gmail.com>
2019-05-17 16:00:50 +0300

    Add project files.

---
+++ SPR-22/DrunkAnimal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace SPR_22
+{
+    class DrunkAnimal
+    {
+        private string name;
+        private int health;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Health
+        {
+            get { return health; }
+            private set
+            {
+                if (value > 100)
+                {
+                    health = 100;
+                }
+                else if (value < 0)
+                {
+                    health = 0;
+                }
+                else
+                {
+                    health = value;
+                }
+            }
+        }
+        public DrunkAnimal()
+        {
+            this.Name = string.Empty;
+            this.Health = 100;
+        }
+
+        public DrunkAnimal(string name) : this()
+        {
+            this.Name = name;
+        }
+
+        public bool IsDead()
+        {
+            return health <= 0;
+        }
+
+        public void Attack(DrunkAnimal enemy)
+        {
+            int damage = new Random().Next(1, 20);
+            enemy.TakeDamage(damage);
+            Console.WriteLine($"{this.name} hit {enemy.name} for {damage} damage ({enemy.health}% hp left)!");
+
+            if (enemy.health == 0)
+            {
+                Console.WriteLine($"{this.name} killed {enemy.name}...");
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            this.Health -= damage;
+        }
+
+        public void Heal(int health)
+        {
+            this.Health += health;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} with {health}% hp";
+        }
+    }
+}
---
+++ SPR-22/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SPR_22
+{
+    class Program
+    {
+        private static ConcurrentQueue<DrunkAnimal> drunks;
+        private static ConcurrentBag<Task> fights = new ConcurrentBag<Task>();
+        private static volatile bool theFightGoesOn = true;
+        private static int fightsCount = 0;
+
+        static void Main()
+        {
+            Console.Write("Enter tournament size between 4 and 32: ");
+
+            int tournamentSize;
+            while (!int.TryParse(Console.ReadLine(), out tournamentSize)
+                || tournamentSize < 4
+                || tournamentSize > 32)
+            {
+                Console.Write("Incorrect input, try again: ");
+            }
+
+            drunks = new ConcurrentQueue<DrunkAnimal>();
+            for (int i = 0; i < tournamentSize; i++)
+            {
+                drunks.Enqueue(new DrunkAnimal($"Fighter {i + 1}"));
+            }
+
+            // the judge stops drinking and starts watching
+            Task.Run(() => Judge());
+            while (theFightGoesOn)
+            {
+                // poor guy has to wait for someone to stop fighting so he can join
+                if (drunks.Count < 2)
+                {
+                    // lets not loop toooooooo fast
+                    Thread.Sleep(100);
+                    continue;
+                }
+
+                drunks.TryDequeue(out DrunkAnimal drunk);
+                drunks.TryDequeue(out DrunkAnimal anotherDrunk);
+
+                fights.Add(Task.Run(() => Fight(drunk, anotherDrunk)));
+            }
+
+            drunks.TryDequeue(out DrunkAnimal winner);
+
+            Console.WriteLine();
+            Console.WriteLine($"Match is over, the winner is: {winner.Name}");
+            Console.WriteLine($"Total fights: {fightsCount}");
+        }
+        
+        // the judge of the fight
+        private static void Judge()
+        {
+            int retries = 0;
+            while (retries < 5)
+            {
+                if (fights.All(x => x.IsCompleted))
+                {
+                    Thread.Sleep(1 * 1000);
+                    retries++;
+                    continue;
+                }
+                retries = 0;
+            }
+
+            theFightGoesOn = false;
+        }
+
+        private static void Fight(DrunkAnimal drunk, DrunkAnimal anotherDrunk)
+        {
+            Console.WriteLine($"{Environment.NewLine}{drunk.Name} CHALLENGED {anotherDrunk.Name}'s mother");
+
+            int turn = 1;
+            while (!drunk.IsDead() && !anotherDrunk.IsDead())
+            {
+                if (turn % 2 == 0)
+                {
+                    drunk.Attack(anotherDrunk);
+                }
+                else
+                {
+                    anotherDrunk.Attack(drunk);
+                }
+
+                Thread.Sleep(1 * 1000);
+                turn++;
+            }
+
+            DrunkAnimal survivor = drunk.IsDead() ? anotherDrunk : drunk;
+            survivor.Heal(1337);
+            drunks.Enqueue(survivor);
+
+            Interlocked.Increment(ref fightsCount);
+        }
+    }
+}
